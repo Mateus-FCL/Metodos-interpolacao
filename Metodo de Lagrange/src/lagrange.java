9984a9c8431adaa09dd2dbb14b48782006059c6b
Mateus-FCL <F4lcomate@gmail.com>
2023-08-30 17:56:07 -0300

    metodo de lagrange

---
+++ Metodo de Lagrange/src/lagrange.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class lagrange {
+
+    public static double interpolate(double[] xValues, double[] yValues, double x) {
+        if (xValues.length != yValues.length) {
+            throw new IllegalArgumentException("OS arrays devem ter o mesmo tamanho");  
+        }
+
+        double result = 0;
+
+        for (int i = 0; i < xValues.length; i++) {
+            double term = yValues[i];
+            for (int j = 0; j < xValues.length; j++) {
+                if (j != i) {
+                    term *= (x - xValues[j]) / (xValues[i] - xValues[j]);
+                }
+            }
+            result += term;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<Double> valuexList = new ArrayList<>();
+        List<Double> valueyList = new ArrayList<>();
+        
+        //Para informar o valor de z
+        System.out.println("Informe o valor de Z: ");
+        double z = in.nextDouble();
+
+        //Para informar os valores de x
+        System.out.println("Informe quantos números vai ter em X: ");
+        int sizeX = in.nextInt();
+        for(int i = 0; i < sizeX; i++){
+            System.out.println("Valor de X: ");
+            double xValues = in.nextDouble();
+            valuexList.add(xValues);
+        }
+
+        //Para informar os valores de y
+        System.out.println("Informer quantos números vai ter em Y: ");
+        int sizeY = in.nextInt();
+        for(int i = 0; i < sizeY; i++){
+            System.out.println("Valor de Y: ");
+            double yValues = in.nextDouble();
+            valueyList.add(yValues);
+        }
+
+        double[] xValues = valuexList.stream().mapToDouble(Double::doubleValue).toArray();
+        double[] yValues = valueyList.stream().mapToDouble(Double::doubleValue).toArray();
+        double interpo = interpolate(xValues, yValues, z);
+        System.out.println("Valor interpolado de z = " + z + " é = " + interpo);
+        in.close();
+    }
+}
